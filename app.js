ff806304e866c3e19480713f92d74fb3085a1b71
CodeMangler <hsdpal@gmail.com>
2011-10-23 02:59:05 +0530

    Can server static resources, but a http.request(...) fails silently, killing the node process..

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
-var fcgiApp = require("./fcgi"),
+var // fcgiApp = require("./fcgi"),
 	http = require("http"),
 	fs = require('fs'),
 	path = require('path'),
-	url = require('url');
+	url = require('url'),
+	util = require('util');
 
 var STATIC_ROOT = path.join(__dirname, 'public_html');
 
+function value(variable, defaultValue) {
+	return variable ? variable : defaultValue;
+}
+
 var proxy = http.createServer(function(req, res) {
-	if(req.path.length <= 1 && req.path.charAt(0) == '/') { // Empty path
-		// serve the index page
-		var indexPath = path.join(STATIC_ROOT, 'index.html');
-		var stat = fs.statSync(indexPath);
+	// DEFECT: req.url doesn't have the hash parameters.. i.e. requesting foo.bar/path#hash results in a request that has only foo.bar/path (#hash stripped off)
+	var requestUrl = req.url.charAt(0) == '/' ? req.url.substr(1) : req.url;
+	if(requestUrl == '')
+		requestUrl = 'index.html'; // Default resource to serve
+	
+	var staticPath = path.join(STATIC_ROOT, requestUrl);
+	var isStaticResource = path.existsSync(staticPath);
+	if(isStaticResource) {
+		// serve the resource
+		var stat = fs.statSync(staticPath);
 		
 		res.writeHead(200, {
 			'Content-Type': 'text/html',
 			'Content-Length': stat.size
 		});
 		
-		var contents = fs.createReadStream(indexPath);
+		var contents = fs.createReadStream(staticPath);
 		contents.on('data', function(chunk) { res.write(chunk); });
 		contents.on('end', function() { res.end(); });
 	} else {
 		// extract the target url
-		var requestUrl =req.path.charAt(0) == '/' ? req.path.substr(1) : req.path;
 		var parsedRequestUrl = url.parse(requestUrl);
-		
+
 		// make a request to the url, replicating headers + method
 		var requestOptions = {
 			host: parsedRequestUrl.host,
-			path: parsedRequestUrl.hash,
+			port: 80, // Replace with a protocol:port map lookup
+			path: value(parsedRequestUrl.pathname, '/') + value(parsedRequestUrl.search, '') + value(parsedRequestUrl.hash, ''),
 			method: req.method
 		};
-		
+		console.log(requestOptions);
 		http.request(requestOptions, function(proxyRes) {
+			console.log('Making a request to: ' + req.url);
 			// write the received content back to user, rewriting as neccessary
 			res.writeHead(proxyRes.statusCode, proxyRes.headers);
 			proxyRes.on('data', function(chunk) { res.write(chunk); });
